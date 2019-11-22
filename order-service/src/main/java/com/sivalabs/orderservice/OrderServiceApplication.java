34424ca9169768d94be1181903f63bc8ba455e59
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-11-22 18:10:42 +0200

    Initial

--- order-service/src/main/java/com/sivalabs/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sivalabs/orderservice/OrderServiceApplication.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableCircuitBreaker
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
